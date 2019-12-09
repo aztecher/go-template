8e51b80536b229dfca9e98fb384a2d47f9d5e58c
aztecher <mikiyaf.business@gmail.com>
2019-12-09 21:55:36 +0900

    [feat] implement very, very basic mysql-template

--- db/mysql/main.go
+++ db/mysql/main.go
@@ -7,8 +7,9 @@
 package main
 
 import (
-	// "database/sql"
+	"database/sql"
 	"fmt"
+	"log"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -29,4 +30,30 @@ type Person struct {
 
 func main() {
 	fmt.Printf("mysql example\n")
+
+	// to connect mysql, determine driver name 'mysql' and username, datasource
+	// [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	db, err := sql.Open("mysql", "root:mysql@tcp(127.0.0.1:3306)/test_template")
+	defer db.Close()
+	log.Println("Connected to mysql.")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// send query and get data
+	rows, err := db.Query("SELECT * FROM person")
+	defer rows.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for rows.Next() {
+		var person Person
+		err := rows.Scan(&person.ID, &person.Name)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(person.ID, person.Name)
+	}
 }
