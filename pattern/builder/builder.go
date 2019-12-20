9a28fcb380685032ee4a91458a3be79059286620
aztecher <mikiyaf.business@gmail.com>
2019-12-20 22:43:02 +0900

    [add] create strange code

--- pattern/builder/builder.go
+++ pattern/builder/builder.go
@@ -4,9 +4,22 @@ import (
 	"fmt"
 )
 
+type Continuation interface {
+	Continue(f func(structure *Structure) Continuation) Continuation
+	End() *Structure
+}
+
 type Structure struct {
 	Id    int
 	Field string
+}
+
+func (structure *Structure) Continue (f func(structure *Structure) Continuation) Continuation {
+	return f(structure)
+}
+
+func (structure *Structure) End () *Structure {
+	return structure
 }
 
 func NewStructure() *Structure {
@@ -31,4 +44,17 @@ func main() {
 	// simple builder
 	structure := NewStructure().SetId(1).SetField("field").Build()
 	fmt.Printf("%v\n", structure)
+
+	// なんかできた
+	// upcast, downcastはinterface実装側で持たせたい気持ちがある
+	// もうちょっとなんか考えられそう
+	strcont := NewStructure().Continue(func (structure *Structure) Continuation {
+		structure.SetId(1)
+		return (interface{}(structure)).(Continuation)
+	}).Continue(func (structure *Structure) Continuation {
+		structure.SetField("field")
+		return (interface{}(structure)).(Continuation)
+	}).End()
+
+	fmt.Printf("%v\n", strcont)
 }
