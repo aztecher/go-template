13886590bc0c785905423bfcc888e29fd203eb71
aztecher <mikiyaf.business@gmail.com>
2019-12-20 23:16:24 +0900

    [add] Implementing Builder Pattern with call method constraint using Interface and tagless final

--- pattern/builder/builder.go
+++ pattern/builder/builder.go
@@ -1,26 +1,32 @@
 package main
 
+// Implementing Builder Pattern with call method constraint using Interface and tagless final
+
 import (
 	"fmt"
 )
 
+// abstract the builder pattern's base structure
 type Continuation interface {
-	Continue(f func(structure *Structure) *Structure) Continuation
-	End() *Structure
+	Continue(f func(structureSetter StructureSetter) *Structure) Continuation
+	Build() *Structure
 }
 
+// Constraint of call method
+type StructureSetter interface {
+	SetId(id int) *Structure
+	SetField(field string) *Structure
+}
+
+// target for build
 type Structure struct {
 	Id    int
 	Field string
 }
 
-func (structure *Structure) Continue (f func(structure *Structure) *Structure) Continuation {
-	str := f(structure)
+func (structure *Structure) Continue (f func(strSetter StructureSetter) *Structure) Continuation {
+	str := f(interface{}(structure).(StructureSetter))
 	return (interface{}(str)).(Continuation)
-}
-
-func (structure *Structure) End () *Structure {
-	return structure
 }
 
 func NewStructure() *Structure {
@@ -41,18 +47,24 @@ func (structure *Structure) Build() *Structure {
 	return structure
 }
 
+func (structure *Structure) TestMethod() *Structure{
+	return nil
+}
+
 func main() {
 	// simple builder
 	structure := NewStructure().SetId(1).SetField("field").Build()
+	// if you call TestMethod(), segumentation fault. but compiler don't catch it. like bellow
+	// structure := NewStructure().SetId(1).TestMethod().SetField("field").Build()
 	fmt.Printf("%v\n", structure)
 
-	// 引数に与える関数の引数と返り値が同一型を取れるように変更でき、
-	// かつコード側の責務が減った(builderのsetter呼びに近い形になった)
-	strcont := NewStructure().Continue(func (structure *Structure) *Structure{
-		return structure.SetId(1)
-	}).Continue(func (structure *Structure) *Structure {
+	// builder with call method constraint
+	// TestMethod() can't suggest and if you call it then compiler caught it as error.
+	strcont := NewStructure().Continue(func (structureSetter StructureSetter) *Structure{
+		return structureSetter.SetId(1)
+	}).Continue(func (structureSetter StructureSetter) *Structure {
 		return structure.SetField("field")
-	}).End()
+	}).Build()
 
 	fmt.Printf("%v\n", strcont)
 }
